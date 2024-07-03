a22830985a5abab162d790e2354760ebfc365bda
Abhay Dange <121017345+kd1-AbhayDange-83587@users.noreply.github.com>
2024-07-03 20:17:24 +0530

    Time: 1 ms (79.28%), Space: 44.4 MB (91.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,6 +1,24 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashSet<Integer> set = new HashSet();
+        int j = 1;
+        int i = 0;
+        int count = 1;
+        while(j < nums.length){
+            if(nums[i] != nums[j]){
+                nums[i+1] = nums[j];
+                i = i + 1;
+                count++;
+            }
+            else{
+                j++;
+            }
+        }
+    return count;
+    }
+    
+}
+
+/*  HashSet<Integer> set = new HashSet();
         for(int i = 0; i < nums.length ; i++){
             set.add(nums[i]);
         }
@@ -10,5 +28,4 @@ class Solution {
         for(int ele: a)
             nums[i++] = ele;
         return a.size();
-    }
-}
+*/
